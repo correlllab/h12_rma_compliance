415f9bc9f00f6fe32d7160e23e47c903a38054cd
Niraj Pudasaini <np2289@nyu.edu>
2025-12-09 13:03:34 -0700

    Initial commit

---
+++ source/h12_rma_compliance/h12_rma_compliance/tasks/manager_based/h12_rma_compliance/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-H12-Rma-Compliance-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.h12_rma_compliance_env_cfg:H12RmaComplianceEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
+    },
+)
